4ec9e4eb6d98ffda44dc6d4cbe4440b6f56430c6
Jonatan Waern <jonatan.waern@intel.com>
2025-03-21 14:47:54 +0100

    Fix up the log levels of debugs in communication
    Signed-off-by: Jonatan Waern <jonatan.waern@intel.com>

--- src/actions/notifications.rs
+++ src/actions/notifications.rs
@@ -8,7 +8,7 @@ use crate::span::{Span};
 use crate::vfs::{Change, VfsSpan};
 use crate::lsp_data::*;
 
-use log::{debug, trace, warn};
+use log::{debug, warn};
 
 use lsp_types::notification::ShowMessage;
 use lsp_types::request::RegisterCapability;
@@ -93,7 +93,7 @@ impl BlockingNotificationAction for DidChangeTextDocument {
         ctx: &mut InitActionContext,
         out: O,
     ) -> Result<(), ResponseError> {
-        trace!("on_change: {:?}, thread: {:?}", params, thread::current().id());
+        debug!("on_change: {:?}, thread: {:?}", params, thread::current().id());
         if params.content_changes.is_empty() {
             return Ok(());
         }
@@ -162,7 +162,7 @@ impl BlockingNotificationAction for DidChangeConfiguration {
         ctx: &mut InitActionContext,
         out: O,
     ) -> Result<(), ResponseError> {
-        trace!("config change: {:?}", params.settings);
+        debug!("config change: {:?}", params.settings);
         use std::collections::HashMap;
         let mut dups = HashMap::new();
         let mut unknowns = vec![];
