75832af8040c645cba94f27fd4555ac38a8ece15
Jonatan Waern <jonatan.waern@intel.com>
2025-05-28 14:08:30 +0200

    Dont bother the server with unknown configs
    When we get vscode to only send the relevant-for-dls configs
    (whether that is by specifying a new domain for them, or by filtering
    manually) we can revert this
    
    Signed-off-by: Jonatan Waern <jonatan.waern@intel.com>

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -106,7 +106,7 @@ impl BlockingRequestAction for ShutdownRequest {
     }
 }
 
-pub(crate) fn maybe_notify_unknown_configs<O: Output>(out: &O, unknowns: &[String]) {
+pub(crate) fn maybe_notify_unknown_configs<O: Output>(_out: &O, unknowns: &[String]) {
     use std::fmt::Write;
     if unknowns.is_empty() {
         return;
@@ -117,10 +117,11 @@ pub(crate) fn maybe_notify_unknown_configs<O: Output>(out: &O, unknowns: &[Strin
         write!(msg, "{}`{}` ", if first { ' ' } else { ',' }, key).unwrap();
         first = false;
     }
-    out.notify(Notification::<ShowMessage>::new(ShowMessageParams {
-        typ: MessageType::WARNING,
-        message: msg,
-    }));
+    warn!("{}", msg);
+    // out.notify(Notification::<ShowMessage>::new(ShowMessageParams {
+    //     typ: MessageType::WARNING,
+    //     message: msg,
+    // }));
 }
 
 #[allow(dead_code)]
