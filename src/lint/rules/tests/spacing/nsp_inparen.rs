d5827d144df16dcafc3af281251bbcea2372b72f
Juan Vasquez <juan.j.vasquez.alfaro@intel.com>
2025-10-08 14:15:23 -0700

    Rename tests for nsp_inparen and sp_braces to follow convention

--- src/lint/rules/tests/spacing/nsp_inparen.rs
+++ src/lint/rules/tests/spacing/nsp_inparen.rs
@@ -2,14 +2,14 @@ use crate::lint::rules::tests::common::{set_up, assert_snippet};
 use crate::lint::rules::RuleType;
 
 //  NSP.inparen immediately inside parentheses or brackets
-static NO_SPACE_INPAREN_METHOD_FUNC_INDEX_INCORRECT: &str = "
+static METHOD_FUNC_INDEX_INCORRECT: &str = "
 method this_is_some_method( conf_object_t *dummy_obj ) {
     if ( !dummy_obj[ 0 ] )
         return;
 }
 ";
 #[test]
-fn no_space_inparen_method_func_index_incorrect() {
+fn method_func_index_incorrect() {
     let mut rules = set_up();
     let expected_errors = define_expected_errors!(
         RuleType::NspInparen,
@@ -20,27 +20,27 @@ fn no_space_inparen_method_func_index_incorrect() {
         (2, 2, 20, 21),
         (2, 2, 22, 23),
     );
-    assert_snippet(NO_SPACE_INPAREN_METHOD_FUNC_INDEX_INCORRECT, expected_errors, &rules);
+    assert_snippet(METHOD_FUNC_INDEX_INCORRECT, expected_errors, &rules);
     // Test rule disable
     rules.nsp_inparen.enabled = false;
-    assert_snippet(NO_SPACE_INPAREN_METHOD_FUNC_INDEX_INCORRECT, vec![], &rules);
+    assert_snippet(METHOD_FUNC_INDEX_INCORRECT, vec![], &rules);
 }
 
 //  NSP.inparen immediately inside parentheses or brackets
-static NO_SPACE_INPAREN_METHOD_FUNC_INDEX_CORRECT: &str = "
+static METHOD_FUNC_INDEX_CORRECT: &str = "
 method this_is_some_method(conf_object_t *dummy_obj) {
     if (!dummy_obj[0])
         return;
 }
 ";
 #[test]
-fn no_space_inparen_method_func_index_correct() {
+fn method_func_index_correct() {
     let rules = set_up();
-    assert_snippet(NO_SPACE_INPAREN_METHOD_FUNC_INDEX_CORRECT, vec![], &rules);
+    assert_snippet(METHOD_FUNC_INDEX_CORRECT, vec![], &rules);
 }
 
 
-pub static NSP_INPAREN_02: &str = "
+pub static COMPOSITE_ARRAY_DECLARATION_AND_TEMPLATES_INCORRECT: &str = "
 bank some_bank {
     group some_group[ i < ( GROUP_COUNT ) ] {
         register some_reg is ( some_template, another_template ) {
@@ -49,14 +49,20 @@ bank some_bank {
     }
 }
 ";
-    #[test]
-    fn style_check_nsp_inparen_02() {
-        // let mut cfg = LintCfg::default();
-        // let mut rules = instantiate_rules(&cfg);
-        // assert_snippet(NSP_INPAREN_02, 6, &rules);
-        // Test rule disable
-        // cfg.nsp_inparen = None;
-        // rules = instantiate_rules(&cfg);
-        // assert_snippet(NSP_INPAREN_02, 0, &rules);
-
-    }
+#[test]
+fn composite_array_declaration_and_templates_incorrect() {
+    let mut rules = set_up();
+    let expected_errors = define_expected_errors!(
+        RuleType::NspInparen,
+        (2, 2, 21, 22),
+        (2, 2, 27, 28),
+        (2, 2, 39, 40),
+        (2, 2, 41, 42),
+        (3, 3, 30, 31),
+        (3, 3, 62, 63),            
+    );
+    assert_snippet(COMPOSITE_ARRAY_DECLARATION_AND_TEMPLATES_INCORRECT, expected_errors, &rules);
+    // Test rule disable
+    rules.nsp_inparen.enabled = false;
+    assert_snippet(COMPOSITE_ARRAY_DECLARATION_AND_TEMPLATES_INCORRECT, vec![], &rules);
+}
